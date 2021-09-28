0b985f3df49daba35dbc9f927e5be661a2610285
Trevor Behlman <tsbehlman@gmail.com>
2021-09-28 18:51:25 -0400

    Fix inaccurate terminology part 2

--- src/observable.js
+++ src/observable.js
@@ -1,10 +1,10 @@
 import { makeObservable, subscribe } from "./observableUtils.js";
 
 export function tap( sideEffect, source ) {
-	return makeObservable( function tapSetup( dispatch ) {
+	return makeObservable( function tapSetup( next ) {
 		const subscription = subscribe( function tapCallback( value ) {
 			sideEffect( value )
-			dispatch( value );
+			next( value );
 		}, source );
 		
 		return () => subscription.unsubscribe();
@@ -12,9 +12,9 @@ export function tap( sideEffect, source ) {
 }
 
 export function map( mapper, source ) {
-	return makeObservable( function mapSetup( dispatch ) {
+	return makeObservable( function mapSetup( next ) {
 		const subscription = subscribe( function mapCallback( value ) {
-			dispatch( mapper( value ) );
+			next( mapper( value ) );
 		}, source );
 		
 		return () => subscription.unsubscribe();
@@ -22,10 +22,10 @@ export function map( mapper, source ) {
 }
 
 export function filter( filterer, source ) {
-	return makeObservable( function filterSetup( dispatch ) {
+	return makeObservable( function filterSetup( next ) {
 		const subscription = subscribe( function filterCallback( value ) {
 			if( filterer( value ) ) {
-				dispatch( value );
+				next( value );
 			}
 		}, source );
 		
@@ -34,7 +34,7 @@ export function filter( filterer, source ) {
 }
 
 export function sampleWith( sampler, source ) {
-	return makeObservable( function sampleWithSetup( dispatch ) {
+	return makeObservable( function sampleWithSetup( next ) {
 		let sourceValue;
 		
 		const sourceSubscription = subscribe( function sampleCallback( value ) {
@@ -42,7 +42,7 @@ export function sampleWith( sampler, source ) {
 		}, source );
 		
 		const samplerSubscription = subscribe( function sampleWithCallback() {
-			dispatch( sourceValue );
+			next( sourceValue );
 		}, sampler );
 		
 		return () => {
@@ -57,12 +57,12 @@ export function combine( combiner, source1, source2 ) {
 }
 
 export function combineArray( combiner, sources ) {
-	return makeObservable( function combineArraySetup( dispatch ) {
+	return makeObservable( function combineArraySetup( next ) {
 		const subscriptions = [];
 		const values = [];
 		
 		if( sources.length === 0 ) {
-			dispatch( combiner() );
+			next( combiner() );
 		}
 		else {
 			for( let i = 0; i < sources.length; i++ ) {
@@ -72,7 +72,7 @@ export function combineArray( combiner, sources ) {
 					}
 					values[ i ] = value;
 					if( values.length === sources.length ) {
-						dispatch( combiner( ...values ) );
+						next( combiner( ...values ) );
 					}
 				}, sources[ i ] );
 			}
@@ -87,20 +87,20 @@ export function merge( source1, source2 ) {
 }
 
 export function mergeArray( sources ) {
-	return makeObservable( function mergeArraySetup( dispatch ) {
-		const subscriptions = sources.map( source => subscribe( dispatch, source ) );
+	return makeObservable( function mergeArraySetup( next ) {
+		const subscriptions = sources.map( source => subscribe( next, source ) );
 		
 		return () => subscriptions.forEach( subscription => subscription.unsubscribe() );
 	} );
 }
 
 export function switchLatest( source ) {
-	return makeObservable( function switchLatestSetup( dispatch ) {
+	return makeObservable( function switchLatestSetup( next ) {
 		let currentSubscription;
 		
 		const sourceSubscription = subscribe( function switchLatestCallback( newSource ) {
 			currentSubscription && currentSubscription.unsubscribe();
-			currentSubscription = subscribe( dispatch, newSource );
+			currentSubscription = subscribe( next, newSource );
 		}, source );
 		
 		return () => {
