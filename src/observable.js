add8da14fe659c4a62ff28f8f56ac88a1e4156f3
Trevor Behlman <tsbehlman@gmail.com>
2021-10-05 01:11:11 -0400

    Separate custom observable library from the rest

--- src/observable.js
+++ src/observable.js
@@ -1,5 +1,111 @@
-import { makeObservable } from "./observableUtils.js";
-import { noop } from "./utils.js";
+import "./observableUtils.js";
+import { noop, passthrough } from "./utils.js";
+
+export function makeObservable( setup ) {
+	const observers = new Set();
+	let value;
+	let cleanup;
+
+	const observerProxy = {
+		next( newValue ) {
+			if( value === newValue ) {
+				return;
+			}
+			value = newValue;
+			for( const observer of observers ) {
+				observer.next( newValue );
+			}
+		},
+		error( error ) {
+			for( const observer of observers ) {
+				observer.error( error );
+			}
+		},
+		complete() {
+			for( const observer of observers ) {
+				observer.complete();
+			}
+		}
+	};
+
+	return {
+		[Symbol.observable]() {
+			return this;
+		},
+		subscribe( observer ) {
+			const subscription = {
+				closed: false,
+				unsubscribe() {
+					this.closed = true;
+					observers.delete( observer );
+					if( observers.size === 0 && cleanup ) {
+						cleanup();
+					}
+				}
+			};
+
+			observer.error = observer.error || function defaultError( error ) {
+				console.error( "Uncaught observable error", error );
+			};
+			observer.complete = observer.complete || function defaultComplete( error ) {
+				subscription.unsubscribe();
+			};
+
+			if( observers.size === 0 && setup ) {
+				cleanup = setup( observerProxy );
+			}
+			observers.add( observer );
+			observer.next( value );
+			return subscription;
+		}
+	};
+}
+
+export function makeSignal( initialValue, mapper = passthrough ) {
+	const observers = new Set();
+	let value = initialValue;
+
+	return {
+		[Symbol.observable]() {
+			return this;
+		},
+		subscribe( observer ) {
+			if( typeof observer === "function" ) {
+				observer = { next: observer };
+			}
+			observers.add( observer );
+			observer.next( value );
+			return {
+				closed: false,
+				unsubscribe() {
+					this.closed = true;
+					observers.delete( observer );
+				}
+			};
+		},
+		get() {
+			return value;
+		},
+		set( newValue ) {
+			if( typeof newValue === "function" ) {
+				newValue = newValue( value );
+			}
+
+			newValue = mapper( newValue );
+
+			if( value === newValue ) {
+				return;
+			}
+			value = newValue;
+			for( const observer of observers ) {
+				observer.next( newValue );
+			}
+		},
+		toJSON() {
+			return value;
+		}
+	};
+}
 
 export function tap( sideEffect, source ) {
 	return makeObservable( function tapSetup( { next, error, complete } ) {
@@ -161,4 +267,4 @@ export function fromPromise( promise, defaultValue ) {
 	} );
 }
 
-export { isObservable, makeObservable, makeSignal } from "./observableUtils.js";
+export { isObservable } from "./observableUtils.js";
